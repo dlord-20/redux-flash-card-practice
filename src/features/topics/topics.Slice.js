ee5964aefb8f0b12573b5a765ab7123442fb0596
dlord-20 <dthomaslord@gmail.com>
2022-07-07 15:13:00 +0300

    Topics slice created with addTopic action. Action and initalState need to change to be formatted correctly.

---
+++ src/features/topics/topics.Slice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    topics: {
+        topics: {
+
+        },
+        favoriteTopics: {
+
+        }
+    }
+}
+
+export const topicsSlice = createSlice({
+    name: 'topics',
+    initialState: initialState,
+    reducers: {
+        addTopic: (state, acton) => {
+            //Might need to deconstruct this to make it easier to understand what is going on
+            state.topics.topics.push(action.payload);
+            //6.3-6.4 bullets
+        }
+    }
+})
+
+export const selectTopics = (state) => state.topics.topics;
+
+
+// export const { changeName, addCharacter, removeCharacter } = usernameSlice.actions;
+
+// export const selectUsername = (state) => state.username.name;
+
+// export default usernameSlice.reducer;
