bdbbdbe11c38cf089e57b0975ef207b8cb6b98fc
dlord-20 <dthomaslord@gmail.com>
2022-07-09 20:16:00 +0300

    imported topicsSlice to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { topicsSlice } from "../features/topics/topics.Slice";
 
 export default configureStore({
   reducer: {
-    
+
   },
 });
